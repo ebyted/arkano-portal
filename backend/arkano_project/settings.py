d13eb0a6b924f5c8a58c37f7b84e023309f19bcb
Edgar Jimenez <ebyted@gmail.com>
2026-05-09 01:23:56 -0700

    Fix conn

--- backend/arkano_project/settings.py
+++ backend/arkano_project/settings.py
@@ -60,12 +60,26 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'arkano_project.wsgi.application'
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+db_engine = os.getenv('DB_ENGINE', 'sqlite3')
+
+if db_engine == 'postgresql':
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.getenv('DB_NAME', 'db-arkano'),
+            'USER': os.getenv('DB_USER', 'usrdb-arkano'),
+            'PASSWORD': os.getenv('DB_PASSWORD', ''),
+            'HOST': os.getenv('DB_HOST', 'localhost'),
+            'PORT': os.getenv('DB_PORT', '5432'),
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
